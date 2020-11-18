ce4d60a4d9d8b8bec4a739f24ed4644a7894ff0f
Moritz Eckert <m1gh7ym0@gmail.com>
2020-11-18 16:28:02 +0100

    Publishing to gh-pages (publish.sh)

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-  var scroll = $(window).scrollTop();
-  if (scroll > 0) {
-    $("nav").addClass("shadow-sm");
-  } else {
-    $("nav").removeClass("shadow-sm");
-  }
-});
